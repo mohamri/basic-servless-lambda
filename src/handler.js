00a1e72b355b8cd6ec572eb783d86433c4418849
moh amri <moh.amrispark@gmail.com>
2022-03-29 15:08:45 -0500

    Fixed lambda code

--- src/handler.js
+++ src/handler.js
@@ -6,6 +6,3 @@ module.exports.generateRandomNumber = async (event) => {
   return randomNumber;
 };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
